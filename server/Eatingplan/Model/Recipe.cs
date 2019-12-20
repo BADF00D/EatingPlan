0c2c72e64f6f5a788d37dbdcbe37eef367e16508
David Störmer <badf00d@posteo.de>
2019-12-20 19:39:23 +0100

    Defining the api

--- server/Eatingplan/Model/Recipe.cs
+++ server/Eatingplan/Model/Recipe.cs
@@ -13,6 +13,38 @@
         /// Title of recipe
         /// </summary>
         public string Name { get; set; }
-        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RecipePart[] RecipeParts { get; set; }
+    }
+
+
+    public class RecipePart
+    {
+        public long Id { get; set; }
+        public long Name { get; set; }
+        public int? Order { get; set; }
+        public Instruction[] Instructions { get; set; }
+    }
+
+    public class Instruction
+    {
+        public long Id { get; set; }
+    }
+
+    public class IngredientInRecipePart
+    {
+        public RecipePart  RecipePart { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public float Amout { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
     }
 }
